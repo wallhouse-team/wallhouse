2a4117747ce2eb37493c0af29ded237747ee9e7c
Muhammadumar <makeman707@gmail.com>
2025-06-12 23:21:57 +0500

    some changes

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,38 +1,27 @@
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 
-<<<<<<< HEAD
 const MainLayout = () => {
   return (
-    <div className='min-h-screen flex flex-col'>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className='relative flex-1'>
+      <div className="relative flex-1">
         <div
-          className='absolute top-0 left-0 right-0 bottom-0 bg-cover bg-center bg-no-repeat z-0'
+          className="absolute top-0 left-0 right-0 bottom-0 bg-cover bg-center bg-no-repeat z-0"
           style={{
             backgroundImage:
               "url('https://walldesign-tests-jgda.vercel.app/assets/bg-sklad-BclbMkb4.png')",
           }}
         >
-          <div className='w-full h-full bg-black/60 backdrop-blur-sm'></div>
+          <div className="w-full h-full bg-black/60 backdrop-blur-sm"></div>
         </div>
 
-        <main className='relative z-10 p-4 mt-[120px]'>
+        <main className="relative z-10 p-4 mt-[120px]">
           <Outlet />
         </main>
       </div>
     </div>
   );
 };
-=======
-const MainLayout = () => (
-  <>
-    <Navbar />
-    <main className="pt-[105px] min-h-screen">
-      <Outlet />
-    </main>
-  </>
-);
->>>>>>> 9b7b23afe624b4c3910ba07289579f73d98958e6
 
 export default MainLayout;
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,47 +1,28 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Outlet, Link, useLocation } from 'react-router-dom';
-=======
-import { Link } from "react-router-dom";
->>>>>>> 9b7b23afe624b4c3910ba07289579f73d98958e6
+import React from "react";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 const UserPage = () => {
   const location = useLocation();
-  const isRootUserPage = location.pathname === '/user';
+  const isRootUserPage = location.pathname === "/user";
 
   return (
     <>
-      <div className='relative bg-black/60 p-8 rounded-xl text-white text-center'>
+      <div className="relative bg-black/60 p-8 rounded-xl text-white text-center">
         {isRootUserPage && (
-          <h1 className='text-3xl font-bold mb-4'>Welcome to User Page</h1>
+          <h1 className="text-3xl font-bold mb-4">Welcome to User Page</h1>
         )}
 
-        <div className='mb-4'>
-          <Link to='/user/vitrinalar' className='text-blue-400 underline mx-2'>
+        <div className="mb-4">
+          <Link to="/user/vitrinalar" className="text-blue-400 underline mx-2">
             Vitrinalar
           </Link>
 
-<<<<<<< HEAD
-          <Link to='/user/goods' className='text-blue-400 underline mx-2'>
+          <Link to="/user/goods" className="text-blue-400 underline mx-2">
             Goods
           </Link>
         </div>
 
         <Outlet />
-=======
-        <Link
-          to="/goods"
-          className="inline-block mt-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded transition"
-        >
-          Tovarlar
-        </Link>
-        <Link
-          to="/goodsshowcase"
-          className="inline-block mt-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded transition"
-        >
-          Tovarlarni vitrinaga junatish
-        </Link>
->>>>>>> 9b7b23afe624b4c3910ba07289579f73d98958e6
       </div>
     </>
   );
